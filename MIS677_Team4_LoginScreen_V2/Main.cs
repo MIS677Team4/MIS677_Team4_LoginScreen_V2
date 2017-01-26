750f9db5145e528a1321e4bc30110ced9bb0c48e
Ian <Ian@DESKTOP-GHQ9A0D>
2017-01-26 14:36:08 -0600

    Add project files.

---
+++ MIS677_Team4_LoginScreen_V2/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIS677_Team4_LoginScreen_V2
+{
+    public partial class Main : Form
+    {
+        private System.Security.Cryptography.Rfc2898DeriveBytes hasher;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
